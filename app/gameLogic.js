81357c829879341958b62623d242dcad4ad33269
Zhk <94470536+dinevz@users.noreply.github.com>
2022-05-15 14:16:31 +0100

    Start screen working

---
+++ app/gameLogic.js
@@ -0,0 +1,9 @@
+//switch between screens
+let startScreen = document.querySelector('.start-screen');
+let playScreen = document.querySelector('.play-screen');
+let startBtn = document.getElementById('start');
+startBtn.addEventListener('click', (e) => {
+    startScreen.classList.add('hidden');
+    playScreen.classList.remove('hidden');
+
+});
